f25707204881cdbf2e22ce319455daa16e22d987
Sam <samuel_morgan_@outlook.com>
2020-03-16 21:01:21 +0000

    First Fetch Request

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const fs = require('fs');
+var cors = require('cors');
 const app = express();
 const port = 8000;
+
+app.use(cors());
 
 let words;
 fs.readFile('./test.txt', 'utf8', (err, data) => {
@@ -9,6 +12,6 @@ fs.readFile('./test.txt', 'utf8', (err, data) => {
 	words = data;
 });
 
-app.get('/', (req, res) => res.send(words));
+app.get('/', (req, res) => res.send(JSON.stringify(words)));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function App() {
-	return <div className="App">Hi</div>;
+	const [ appText, setText ] = useState('No data from server');
+
+	async function getServerText() {
+		const url = 'http://localhost:8000/';
+		const response = await fetch(url);
+		return response;
+	}
+
+	useEffect(
+		() => {
+			async function setAppText() {
+				const text = await getServerText();
+				const json = await text.json();
+				setText(json);
+			}
+			setAppText();
+		},
+		[ appText ]
+	);
+
+	return <div className="App">{appText}</div>;
 }
 
 export default App;
