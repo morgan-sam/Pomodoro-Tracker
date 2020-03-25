1d06fba4ca37f2079f9d845a39278b34f2f6ce7e
Sam <samuel_morgan_@outlook.com>
2020-03-25 20:20:32 +0000

    Added reset to today btn

--- src/components/DateNavigation.jsx
+++ src/components/DateNavigation.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import DateSelect from 'components/DateSelect';
 import { getISODateXDaysAway } from 'utility/timeFunctions';
 import { parseISOToDateObj, parseDateObjToISO } from 'utility/parseDates';
+import { getDayFromTodayAsISO } from 'data/dates';
 
 const DateNavigation = (props) => {
 	const arrowIconStyle = {
@@ -23,6 +24,20 @@ const DateNavigation = (props) => {
 		verticalAlign: 'middle'
 	};
 
+	const todayBtnStyle = {
+		height: '2rem',
+		width: 'auto',
+		padding: '0 1rem',
+		verticalAlign: 'middle',
+		textAlign: 'center'
+	};
+
+	const centerContainer = {
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center'
+	};
+
 	return (
 		<div style={dateContainerStyle} className={'dateNavigation'}>
 			<button
@@ -35,15 +50,27 @@ const DateNavigation = (props) => {
 			>
 				⬅️
 			</button>
-			<DateSelect
-				date={parseISOToDateObj(props.filterOptions.date)}
-				setDate={(dateObj) => {
-					props.setFilterOptions({
-						...props.filterOptions,
-						date: parseDateObjToISO(dateObj)
-					});
-				}}
-			/>
+			<div style={centerContainer}>
+				<DateSelect
+					date={parseISOToDateObj(props.filterOptions.date)}
+					setDate={(dateObj) => {
+						props.setFilterOptions({
+							...props.filterOptions,
+							date: parseDateObjToISO(dateObj)
+						});
+					}}
+				/>
+				<button
+					style={todayBtnStyle}
+					onClick={() =>
+						props.setFilterOptions({
+							...props.filterOptions,
+							date: getDayFromTodayAsISO()
+						})}
+				>
+					Reset To Today
+				</button>
+			</div>
 			<button
 				style={arrowIconStyle}
 				onClick={() =>
