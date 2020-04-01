558b063734ecc93c0bf8ee25b076d7ab29f67ed0
Sam <samuel_morgan_@outlook.com>
2020-04-01 13:14:30 +0100

    Created TimerEvent Schema/Model

---
+++ backend/models/TimerEvent.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const TimerEventSchema = new Schema({
+	type: String,
+	date: String
+});
+
+const TimerEvent = mongoose.model('timerEvent', TimerEventSchema);
+
+modules.exports = TimerEvent;
