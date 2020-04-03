b48ad18187da5bc0d5af92a1d9e536e02891ebe8
Sam <samuel_morgan_@outlook.com>
2020-04-03 16:07:06 +0100

    canvasDrawText sep file

---
+++ client/src/controller/canvasDrawText.js
@@ -0,0 +1,45 @@
+import { GRAPH_SIZES } from 'styles/graphSizing';
+import { monthStringArray, addOrSubtractDaysFromDateObj } from 'data/dates';
+import { parseBigEndianToObj, parseDateObjToLittleEndian } from 'utility/parseDates';
+const GRAPH_FONT_SIZE = GRAPH_SIZES.FONT_SIZE;
+const GRAPH_BOTTOM_GAP = GRAPH_SIZES.BOTTOM_GAP;
+
+export const drawGraphTitle = (graph) => {
+	graph.context.font = ((GRAPH_FONT_SIZE * 1.2) | 0) + 'px sans-serif';
+	graph.context.fillText(getGraphTitleText(graph), graph.canvasRef.current.width / 2, GRAPH_FONT_SIZE * 2);
+};
+
+export const drawXLabelText = (graph, textLabelObj) => {
+	const { x, dateText, raisedMonthLabel } = textLabelObj;
+	for (let i = 0; i < dateText.length; i++) {
+		graph.context.font = getXAxisFont(graph.period);
+		graph.context.fillText(
+			dateText[i],
+			x,
+			graph.canvasRef.current.height - GRAPH_FONT_SIZE * (i + 2 + raisedMonthLabel)
+		);
+	}
+};
+
+export const drawYLabelText = (graph, textLabelObj) => {
+	const { i, unit } = textLabelObj;
+	graph.context.font = (GRAPH_FONT_SIZE | 0) + 'px sans-serif';
+	graph.context.fillText(i, GRAPH_FONT_SIZE * 2, graph.canvasRef.current.height - GRAPH_BOTTOM_GAP - i * unit);
+};
+
+function getXAxisFont(period) {
+	if (period.match(/week/)) return ((GRAPH_FONT_SIZE / 1.2) | 0) + 'px sans-serif';
+	if (period === 'month') return ((GRAPH_FONT_SIZE / 1.5) | 0) + 'px sans-serif';
+}
+
+function getGraphTitleText(graph) {
+	const dateObj = parseBigEndianToObj(Object.keys(graph.counts)[0]);
+	if (graph.period.match(/week/)) return getWeekGraphTitleRange(dateObj);
+	if (graph.period === 'month')
+		return `Pomodoros in ${monthStringArray[parseInt(dateObj.month) - 1]} ${dateObj.year}`;
+}
+
+function getWeekGraphTitleRange(firstDate) {
+	const secondDate = addOrSubtractDaysFromDateObj(firstDate, 6);
+	return `Pomodoros from ${parseDateObjToLittleEndian(firstDate)} to ${parseDateObjToLittleEndian(secondDate)} `;
+}
--- client/src/controller/canvasDrawing.js
+++ client/src/controller/canvasDrawing.js
@@ -1,5 +1,5 @@
-import { monthStringArray, addOrSubtractDaysFromDateObj } from 'data/dates';
-import { parseBigEndianToObj, parseDateObjToLittleEndian } from 'utility/parseDates';
+import { parseBigEndianToObj } from 'utility/parseDates';
+import { drawGraphTitle, drawXLabelText, drawYLabelText } from 'controller/canvasDrawText';
 import { GRAPH_SIZES } from 'styles/graphSizing';
 const GRAPH_FONT_SIZE = GRAPH_SIZES.FONT_SIZE;
 const GRAPH_BOTTOM_GAP = GRAPH_SIZES.BOTTOM_GAP;
@@ -26,11 +26,6 @@ export const drawEntireGraph = (graphDataObj) => {
 };
 
 /////////////////// MAIN DRAW FUNCTIONS /////////////////
-
-const drawGraphTitle = (graph) => {
-	graph.context.font = ((GRAPH_FONT_SIZE * 1.2) | 0) + 'px sans-serif';
-	graph.context.fillText(getGraphTitleText(graph), graph.canvasRef.current.width / 2, GRAPH_FONT_SIZE * 2);
-};
 
 const drawXAxis = (graph) => {
 	graph.graphData.forEach((el, i) => {
@@ -61,21 +56,20 @@ function getXAxisLabelText(period, date) {
 	if (period === 'month') return [ `${parseInt(dateObj.day)}` ];
 }
 
-function getXAxisFont(period) {
-	if (period.match(/week/)) return ((GRAPH_FONT_SIZE / 1.2) | 0) + 'px sans-serif';
-	if (period === 'month') return ((GRAPH_FONT_SIZE / 1.5) | 0) + 'px sans-serif';
+function getXAxisLabelObj(el, i, period) {
+	return {
+		x: el.coordinate.x,
+		dateText: getXAxisLabelText(period, el.date).reverse(),
+		raisedMonthLabel: (period === 'month') * (i % 2)
+	};
 }
 
-function getGraphTitleText(graph) {
-	const dateObj = parseBigEndianToObj(Object.keys(graph.counts)[0]);
-	if (graph.period.match(/week/)) return getWeekGraphTitleRange(dateObj);
-	if (graph.period === 'month')
-		return `Pomodoros in ${monthStringArray[parseInt(dateObj.month) - 1]} ${dateObj.year}`;
-}
+//////////// Draw Lines ////////////
 
-function getWeekGraphTitleRange(firstDate) {
-	const secondDate = addOrSubtractDaysFromDateObj(firstDate, 6);
-	return `Pomodoros from ${parseDateObjToLittleEndian(firstDate)} to ${parseDateObjToLittleEndian(secondDate)} `;
+function drawPassedLinePath(context, lineFn) {
+	context.beginPath();
+	lineFn(context);
+	context.stroke();
 }
 
 function drawGraphLine(graph) {
@@ -96,44 +90,12 @@ function drawCoordinateCrosses(graph, size) {
 	});
 }
 
-function drawPassedLinePath(context, lineFn) {
-	context.beginPath();
-	lineFn(context);
-	context.stroke();
-}
-
-function getXAxisLabelObj(el, i, period) {
-	return {
-		x: el.coordinate.x,
-		dateText: getXAxisLabelText(period, el.date).reverse(),
-		raisedMonthLabel: (period === 'month') * (i % 2)
-	};
-}
-
 function drawXLabelLine(graph, lineLabelObj) {
 	const { x, raisedMonthLabel } = lineLabelObj;
 	drawPassedLinePath(graph.context, (ctx) => {
 		ctx.moveTo(x, graph.canvasRef.current.height);
 		ctx.lineTo(x, graph.canvasRef.current.height - GRAPH_FONT_SIZE * (1 + raisedMonthLabel));
 	});
-}
-
-function drawXLabelText(graph, textLabelObj) {
-	const { x, dateText, raisedMonthLabel } = textLabelObj;
-	for (let i = 0; i < dateText.length; i++) {
-		graph.context.font = getXAxisFont(graph.period);
-		graph.context.fillText(
-			dateText[i],
-			x,
-			graph.canvasRef.current.height - GRAPH_FONT_SIZE * (i + 2 + raisedMonthLabel)
-		);
-	}
-}
-
-function drawYLabelText(graph, textLabelObj) {
-	const { i, unit } = textLabelObj;
-	graph.context.font = (GRAPH_FONT_SIZE | 0) + 'px sans-serif';
-	graph.context.fillText(i, GRAPH_FONT_SIZE * 2, graph.canvasRef.current.height - GRAPH_BOTTOM_GAP - i * unit);
 }
 
 function drawYLabelLine(graph, lineLabelObj) {
@@ -143,3 +105,5 @@ function drawYLabelLine(graph, lineLabelObj) {
 		ctx.lineTo(GRAPH_FONT_SIZE, graph.canvasRef.current.height - GRAPH_BOTTOM_GAP - i * unit);
 	});
 }
+
+///////////////////////////////////////////////////
