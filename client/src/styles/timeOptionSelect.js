29cc24725536350226c1cde8230e4f8e4522234d
Sam <samuel_morgan_@outlook.com>
2020-04-03 20:58:39 +0100

    Removed old unused container styles

--- client/src/styles/timeOptionSelect.js
+++ client/src/styles/timeOptionSelect.js
@@ -1,12 +1,3 @@
-export const containerStyle = {
-	display: 'grid',
-	gap: '1rem',
-	gridAutoFlow: 'column',
-	gridTemplateColumns: '5rem 6rem 5rem 6rem',
-	zIndex: '1',
-	padding: '2rem 0'
-};
-
 export const timeOptionLabelStyle = {
 	display: 'flex',
 	justifyContent: 'center',
--- client/src/styles/timelineZoomSelect.js
+++ client/src/styles/timelineZoomSelect.js
@@ -1,12 +1,3 @@
-export const containerStyle = {
-	display: 'grid',
-	gap: '1rem',
-	gridAutoFlow: 'column',
-	gridTemplateColumns: '8rem 15rem 7rem 6rem 1rem',
-	zIndex: '1',
-	padding: '1rem 0'
-};
-
 export const timelineZoomLabelStyle = {
 	display: 'flex',
 	justifyContent: 'center',
