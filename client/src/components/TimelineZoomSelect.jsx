5fdda016e307fd72c86822cc4d1dd0b28c60b109
Sam <samuel_morgan_@outlook.com>
2020-04-03 16:54:28 +0100

    Reset Zoom Btn

--- client/src/components/TimelineZoomSelect.jsx
+++ client/src/components/TimelineZoomSelect.jsx
@@ -20,9 +20,23 @@ function TimelineZoomSelect(props) {
 				step="0.01"
 				style={{ width: 'auto' }}
 			/>
-			<BounceButton />
+			<BounceButton
+				onClick={() => {
+					const hourRange = props.timeOptions.endTime - props.timeOptions.startTime;
+					props.setTimeOptions({
+						...props.timeOptions,
+						hourWidth: getPageWidth() / hourRange - 0.5
+					});
+				}}
+				delay={350}
+				text={'Reset Zoom'}
+			/>
 		</div>
 	);
+}
+
+function getPageWidth() {
+	return window.innerWidth / parseFloat(getComputedStyle(document.querySelector('body'))['font-size']);
 }
 
 export default TimelineZoomSelect;
