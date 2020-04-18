54354e80f2611ff39d6aae58a96ee8730c666706
Sam <samuel_morgan_@outlook.com>
2020-04-18 21:06:48 +0100

    Deleted backend apart from model

--- config/keys.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	Host: 'ec2-18-233-137-77.compute-1.amazonaws.com',
-	Database: 'd5q0393mpjgvvk',
-	User: 'oydowpgwepmfmp',
-	Port: 5432,
-	Password: '195958def3d4e4687a4a3bfc59b70276a0973f3f127fdf905cbc4c02fcf71329',
-	URI:
-		'postgres://oydowpgwepmfmp:195958def3d4e4687a4a3bfc59b70276a0973f3f127fdf905cbc4c02fcf71329@ec2-18-233-137-77.compute-1.amazonaws.com:5432/d5q0393mpjgvvk'
-};
