7fd5a771909f16edd18871942c4390675ffabd2c
Sam <samuel_morgan_@outlook.com>
2020-04-24 21:24:52 +0100

    Grid fits page in both chrome/ff

--- client/src/styles/graphPanel.js
+++ client/src/styles/graphPanel.js
@@ -1,7 +1,7 @@
 export const graphStyle = {
 	display: 'flex',
-	height: '100%',
-	width: '100%',
 	border: '1px solid black',
-	margin: '2rem 0'
+	boxSizing: 'border-box',
+	alignItems: 'center',
+	justifyContent: 'center'
 };
