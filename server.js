da1e955e3bdb3abf484354b3eb20857fba1ea112
Sam <samuel_morgan_@outlook.com>
2020-04-24 21:47:32 +0100

    Added express.static

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use(
 );
 app.use(cors());
 
+app.use(express.static(path.join(__dirname, 'client/build')));
 app.get('*', (req, res) => {
 	res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
