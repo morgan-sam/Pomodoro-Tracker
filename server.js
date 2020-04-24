3ef8020195cb65ec54f02648a28f9ffd3d2d9857
Sam <samuel_morgan_@outlook.com>
2020-04-24 21:49:35 +0100

    Removed body parser

--- server.js
+++ server.js
@@ -1,16 +1,9 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
 const db = require('./config/queries');
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(
-	bodyParser.urlencoded({
-		extended: true
-	})
-);
 app.use(cors());
 
 app.use(express.static(path.join(__dirname, 'client/build')));
