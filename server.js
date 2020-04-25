f16de27b044dc53e6c0bbbf2dbcb51e02eaa20f5
Sam <samuel_morgan_@outlook.com>
2020-04-25 22:43:02 +0100

    Added json parser

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const db = require('./config/queries');
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 app.get('/', (req, res) => {
