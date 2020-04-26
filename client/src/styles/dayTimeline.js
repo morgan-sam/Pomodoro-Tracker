dadc7abe0d85a48390281e2cac17bd6b27fdd209
Sam <samuel_morgan_@outlook.com>
2020-04-26 21:53:17 +0100

    Made time marker a zig zag

--- client/src/styles/dayTimeline.js
+++ client/src/styles/dayTimeline.js
@@ -69,9 +69,17 @@ export const eventBoxTypeStyle = {
 	}
 };
 
+const zzwidth = 3;
+const zzcolor = '#FF7744';
 export const currentTimeMarkerStyle = {
-	width: '1px',
-	height: `calc(${EVENT_HEIGHT_REM + 3}rem)`,
-	backgroundColor: 'red',
-	position: 'absolute'
+	width: `calc(${EVENT_HEIGHT_REM + 3}rem - 2px`,
+	height: `${zzwidth * 2}px`,
+	background: `linear-gradient(-45deg, ${zzcolor} ${zzwidth}px, transparent 0), linear-gradient(45deg, ${zzcolor} ${zzwidth}px, transparent 0)`,
+	backgroundSize: `${zzwidth * 2}px ${zzwidth * 2}px`,
+	backgroundPosition: 'left-center',
+	transformOrigin: `-1px 0%`,
+	position: 'absolute',
+	top: '0',
+	right: '0',
+	transform: `rotate(90deg) translateY(-${zzwidth * 2}px)`
 };
