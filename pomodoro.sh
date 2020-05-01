a4ba1f7292beea438265d37152e024082452027a
Sam <samuel_morgan_@outlook.com>
2020-05-01 18:45:47 +0100

    Removed python dependency

--- pomodoro.sh
+++ pomodoro.sh
@@ -12,13 +12,13 @@ while [ "$date1" -ge `date +%s` ]; do
   echo -ne "$(date -u --date @$(($date1 - `date +%s` )) +%H:%M:%S)\r"; 
 done
 clear
-python ./notification/finish.py
+notify-send "Pomodoro Complete" "$(echo -e "25 minutes have passed")"
 curl -X POST -d "{\"type\": \"pomodoro\"}" -H "Content-Type: application/json" https://pomodoro-tracker-app.herokuapp.com/entries
 seconds=300; date1=$((`date +%s` + $seconds)); 
 echo -ne 'Encore:\n'
 while [ "$date1" -ge `date +%s` ]; do 
   echo -ne "$(date -u --date @$(($date1 - `date +%s` )) +%H:%M:%S)\r"; 
 done
-python ./notification/encore.py
+notify-send "Encore Complete" "$(echo -e "5 minutes have passed")"
 curl -X POST -d "{\"type\": \"encore\"}" -H "Content-Type: application/json" https://pomodoro-tracker-app.herokuapp.com/entries
 
