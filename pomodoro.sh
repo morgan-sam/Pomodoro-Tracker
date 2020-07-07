2d8e19d2415497c6d7727e8011f2a8503841bf48
Sam <samuel_morgan_@outlook.com>
2020-07-07 12:22:21 +0100

    Updated script to curl firebase

--- pomodoro.sh
+++ pomodoro.sh
@@ -5,7 +5,9 @@ for pid in $(pidof -x $script_name); do
         kill -9 $pid
     fi 
 done
-curl -X POST -d "{\"type\": \"start\"}" -H "Content-Type: application/json" https://pomodoro-tracker-app.herokuapp.com/entries
+TIME="$(date -Iseconds)Z";
+ENTRY='{"type":"start","date":"'$TIME'"}';
+curl -X POST -d $ENTRY 'https://pomodoro-tracker-db95f.firebaseio.com/users/23456789/events.json'
 seconds=1500; date1=$((`date +%s` + $seconds)); 
 echo -ne 'Pomodoro:\n'
 while [ "$date1" -ge `date +%s` ]; do 
@@ -13,12 +15,15 @@ while [ "$date1" -ge `date +%s` ]; do
 done
 clear
 notify-send "Pomodoro Complete" "$(echo -e "25 minutes have passed")"
-curl -X POST -d "{\"type\": \"pomodoro\"}" -H "Content-Type: application/json" https://pomodoro-tracker-app.herokuapp.com/entries
+TIME="$(date -Iseconds)Z";
+ENTRY='{"type":"pomodoro","date":"'$TIME'"}';
+curl -X POST -d $ENTRY 'https://pomodoro-tracker-db95f.firebaseio.com/users/23456789/events.json'
 seconds=300; date1=$((`date +%s` + $seconds)); 
 echo -ne 'Encore:\n'
 while [ "$date1" -ge `date +%s` ]; do 
   echo -ne "$(date -u --date @$(($date1 - `date +%s` )) +%H:%M:%S)\r"; 
 done
 notify-send "Encore Complete" "$(echo -e "5 minutes have passed")"
-curl -X POST -d "{\"type\": \"encore\"}" -H "Content-Type: application/json" https://pomodoro-tracker-app.herokuapp.com/entries
-
+TIME="$(date -Iseconds)Z";
+ENTRY='{"type":"encore","date":"'$TIME'"}';
+curl -X POST -d $ENTRY 'https://pomodoro-tracker-db95f.firebaseio.com/users/23456789/events.json'
