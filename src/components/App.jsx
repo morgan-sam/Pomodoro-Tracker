170e4ec4fdb0ba07e5f65f34d38415d4a24d18e7
Sam <samuel_morgan_@outlook.com>
2020-07-09 12:57:07 +0100

    Using AuthProvider in app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 import Main from 'components/Main';
 import LoginSignUp from 'components/LoginSignUp';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { AuthProvider } from 'context/auth';
 
 const App = () => {
 	return (
-		<Router>
-			<Route exact path="/" component={Main} />
-			<Route exact path="/login" render={(props) => <LoginSignUp type="login" {...props} />} />
-			<Route exact path="/signup" render={(props) => <LoginSignUp type="signup" {...props} />} />
-		</Router>
+		<AuthProvider>
+			<Router>
+				<Route exact path="/" component={Main} />
+				<Route exact path="/login" render={(props) => <LoginSignUp type="login" {...props} />} />
+				<Route exact path="/signup" render={(props) => <LoginSignUp type="signup" {...props} />} />
+			</Router>
+		</AuthProvider>
 	);
 };
 
