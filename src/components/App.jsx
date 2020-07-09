de051e459cfadae36e97b09bac89955c9482d0fb
Sam <samuel_morgan_@outlook.com>
2020-07-09 11:19:18 +0100

    LoginSignup one component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import Main from 'components/Main';
-import Login from 'components/Login';
-import SignUp from 'components/SignUp';
+import LoginSignUp from 'components/LoginSignUp';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 const App = () => {
 	return (
 		<Router>
 			<Route exact path="/" component={Main} />
-			<Route exact path="/login" component={Login} />
-			<Route exact path="/signup" component={SignUp} />
+			<Route exact path="/login" render={(props) => <LoginSignUp type="login" {...props} />} />
+			<Route exact path="/signup" render={(props) => <LoginSignUp type="signup" {...props} />} />
 		</Router>
 	);
 };
--- src/components/LoginSignUp.jsx
+++ src/components/LoginSignUp.jsx
@@ -34,19 +34,25 @@ const footerLinkStyle = {
 };
 
 const handleLogin = () => null;
+const handleSignUp = () => null;
 
-const Login = () => {
+const LoginSignup = (props) => {
+	const { type } = props;
 	return (
 		<div style={screenContainerStyle}>
 			<div style={loginSignUpBoxStyle}>
-				<h3 style={titleStyle}>Login to your account</h3>
-				<Form onSubmit={handleLogin} inputs={[ 'email', 'password' ]} submitText={'Log In'} />
-				<a style={footerLinkStyle} href="/signup">
-					Sign Up
+				<h3 style={titleStyle}>{type === 'login' ? 'Login to your account' : 'Sign up for an account'}</h3>
+				<Form
+					onSubmit={type === 'login' ? handleLogin : handleSignUp}
+					inputs={[ 'email', 'password' ]}
+					submitText={type === 'login' ? 'Log In' : 'Sign Up'}
+				/>
+				<a style={footerLinkStyle} href={type === 'login' ? '/signup' : '/login'}>
+					{type === 'login' ? 'Sign Up' : 'Log In'}
 				</a>
 			</div>
 		</div>
 	);
 };
 
-export default Login;
+export default LoginSignup;
