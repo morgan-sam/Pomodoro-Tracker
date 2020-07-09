3bd14425660422b7d1183353a6e30b28f47ab422
Sam <samuel_morgan_@outlook.com>
2020-07-09 10:31:08 +0100

    login/signup skeleton

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import Main from 'components/Main';
+import Login from 'components/Login';
+import SignUp from 'components/SignUp';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 const App = () => {
-	return <Main />;
+	return (
+		<Router>
+			<div>
+				<Route exact path="/" component={Main} />
+				<Route exact path="/login" component={Login} />
+				<Route exact path="/signup" component={SignUp} />
+			</div>
+		</Router>
+	);
 };
 
 export default App;
