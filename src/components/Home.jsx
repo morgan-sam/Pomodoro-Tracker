4724e7ca8bbe6aa1b24e2cc90cbe3f193f70c516
Sam <samuel_morgan_@outlook.com>
2020-07-09 13:34:00 +0100

    Positioned login/signup links

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,44 @@
 import React from 'react';
 
+const homeStyle = {
+	display: 'flex',
+	width: '100vw',
+	height: '100vh',
+	justifyContent: 'center',
+	boxSizing: 'border-box',
+	padding: '5rem'
+};
+
+const loginSignUpBtnContainerStyle = {
+	position: 'absolute',
+	top: '0',
+	right: '0',
+	margin: '1rem 0.5rem',
+	padding: '1rem'
+};
+
+const loginSignUpBtnStyle = {
+	padding: '0.5rem 1rem',
+	marginLeft: '1rem',
+	textDecoration: 'none',
+	border: '1px solid #444',
+	borderRadius: '5px'
+};
+
 const Home = () => {
-	return <div>TEST HOME</div>;
+	return (
+		<div style={homeStyle}>
+			<h1>Pomodoro Tracker</h1>
+			<div style={loginSignUpBtnContainerStyle}>
+				<a href="/login" style={loginSignUpBtnStyle}>
+					Log In
+				</a>
+				<a href="/signup" style={loginSignUpBtnStyle}>
+					Sign Up
+				</a>
+			</div>
+		</div>
+	);
 };
 
 export default Home;
