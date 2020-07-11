98531f6f20a6d4142cb0f3be57b0b432b8b1e580
Sam <samuel_morgan_@outlook.com>
2020-07-11 10:06:39 +0100

    Added loading state

--- src/context/auth.js
+++ src/context/auth.js
@@ -5,8 +5,13 @@ export const AuthContext = React.createContext();
 
 export const AuthProvider = ({ children }) => {
 	const [ currentUser, setCurrentUser ] = useState(null);
+	const [ loading, setLoading ] = useState(true);
 	useEffect(() => {
-		firebase.auth().onAuthStateChanged(setCurrentUser);
+		firebase.auth().onAuthStateChanged((user) => {
+			setCurrentUser(user);
+			setLoading(false);
+		});
 	}, []);
+	if (loading) return <div>Loading...</div>;
 	return <AuthContext.Provider value={{ currentUser }}>{children}</AuthContext.Provider>;
 };
