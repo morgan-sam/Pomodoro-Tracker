f4cc65460ddc1dadb7a0d18a689fbca55bfc7743
Sam <samuel_morgan_@outlook.com>
2020-07-11 10:53:10 +0100

    account btn cursor pointer

--- src/styles/accountSettings.js
+++ src/styles/accountSettings.js
@@ -3,6 +3,7 @@ export const getAccountButtonStyle = (darkTheme) => {
 		padding: '0.5rem 1rem',
 		borderRadius: '5px',
 		border: darkTheme ? 'none' : '1px solid #ccc',
-		boxShadow: '2px 2px #ccc'
+		boxShadow: '2px 2px #ccc',
+		cursor: 'pointer'
 	};
 };
