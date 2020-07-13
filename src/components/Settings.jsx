fc2ce8d64d3ad56c726f92e6deb6836d19fc5b47
Sam <samuel_morgan_@outlook.com>
2020-07-13 22:45:25 +0100

    Converted settings styling to css

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -3,54 +3,16 @@ import { useHistory } from 'react-router-dom';
 import { getSystemButtonStyle } from 'styles/systemSettings';
 import { accountFunctions } from 'controller/accountFunctions';
 
-const screenContainer = {
-	position: 'absolute',
-	top: '0',
-	left: '0',
-	height: '100vh',
-	width: '100vw',
-	display: 'flex',
-	alignItems: 'center',
-	justifyContent: 'center'
-};
-
-const settingsBox = {
-	width: 'fit-content',
-	height: 'fit-content',
-	border: '1px solid black',
-	display: 'flex',
-	alignItems: 'center',
-	borderRadius: '1rem',
-	flexDirection: 'column',
-	padding: '2rem'
-};
-
-const buttonGrid = {
-	display: 'grid',
-	gridTemplateColumns: 'repeat(2, 1fr)',
-	gridTemplateRows: 'repeat(2, 1fr)',
-	gridGap: '1rem',
-	padding: '2rem'
-};
-
-const titleStyle = {
-	padding: '1rem'
-};
-
-const returnButtonContainer = {
-	padding: '1rem'
-};
-
 const Settings = () => {
 	const { changePassword, changeEmail, resetAccount, deleteAccount } = accountFunctions;
 	const history = useHistory();
 	const accountButtonStyle = getSystemButtonStyle(false);
 
 	return (
-		<div style={screenContainer}>
-			<div style={settingsBox}>
-				<h2 style={titleStyle}>Settings</h2>
-				<div style={buttonGrid}>
+		<div className="screenContainer">
+			<div className="settingsBox">
+				<h2 className="header">Settings</h2>
+				<div className="buttonGrid">
 					<button style={accountButtonStyle} onClick={changePassword}>
 						Change Password
 					</button>
@@ -64,7 +26,7 @@ const Settings = () => {
 						Delete Account
 					</button>
 				</div>
-				<div style={returnButtonContainer}>
+				<div className="footer">
 					<button style={accountButtonStyle} onClick={() => history.push('/')}>
 						Return
 					</button>
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'css/home.css';
+import 'css/settings.css';
 import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
