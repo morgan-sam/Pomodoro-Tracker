985548d48fa749dd6bbdad4c47fce017db925f77
Sam <samuel_morgan_@outlook.com>
2020-07-22 18:19:43 +0100

    TimelineSettings comp skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Main from 'screens/Main';
 import Home from 'screens/Home';
 import Settings from 'screens/Settings';
 import AccountSettings from 'screens/AccountSettings';
+import TimelineSettings from 'screens/TimelineSettings';
 import Script from 'components/Script';
 import LoginSignUp from 'components/LoginSignUp';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
@@ -19,6 +20,12 @@ const App = () => {
 				<Route exact path="/signup" render={(props) => <LoginSignUp type="signup" {...props} />} />
 				<AuthRedirectRoute exact path="/settings" AuthComponent={Settings} redirect={'/login'} />
 				<AuthRedirectRoute exact path="/settings/account" AuthComponent={AccountSettings} redirect={'/login'} />
+				<AuthRedirectRoute
+					exact
+					path="/settings/timeline"
+					AuthComponent={TimelineSettings}
+					redirect={'/login'}
+				/>
 				<AuthRedirectRoute exact path="/script" AuthComponent={Script} redirect={'/login'} />
 			</Router>
 		</AuthProvider>
--- src/screens/TimelineSettings.jsx
+++ src/screens/TimelineSettings.jsx
@@ -2,24 +2,19 @@ import React from 'react';
 import { useHistory } from 'react-router-dom';
 import { getSystemButtonStyle } from 'styles/systemSettings';
 
-const Settings = () => {
+const TimelineSettings = () => {
 	const history = useHistory();
 	const accountButtonStyle = getSystemButtonStyle(false);
 
 	return (
 		<div className="screenContainer">
 			<div className="settingsBox">
-				<h2 className="header">Settings</h2>
+				<h2 className="header">Timeline Settings</h2>
 				<div className="button-column">
-					<button style={accountButtonStyle} onClick={() => history.push('/settings/timeline')}>
-						Timeline Settings
-					</button>
-					<button style={accountButtonStyle} onClick={() => history.push('/settings/account')}>
-						Account Settings
-					</button>
+					<div>(timeline settings)</div>
 				</div>
 				<div className="footer">
-					<button style={accountButtonStyle} onClick={() => history.push('/')}>
+					<button style={accountButtonStyle} onClick={() => history.push('/settings')}>
 						Return
 					</button>
 				</div>
@@ -28,4 +23,4 @@ const Settings = () => {
 	);
 };
 
-export default Settings;
+export default TimelineSettings;
