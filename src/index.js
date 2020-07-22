33dc3eecafb7d8e41fee7b7e08d7a9aff932b9fd
Sam <samuel_morgan_@outlook.com>
2020-07-22 18:09:21 +0100

    Added css files to css folder

--- src/index.js
+++ src/index.js
@@ -3,21 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'css/home.css';
 import 'css/settings.css';
-
-import 'styles/main.css';
-import 'styles/dropdown.css';
-import 'styles/form.css';
-import 'styles/timeOptionSelect.css';
-import 'styles/bottomPanel.css';
-import 'styles/dateNavigation.css';
-import 'styles/timelineDisplaySelect.css';
-import 'styles/optionsPanel.css';
-import 'styles/dateSelect.css';
-import 'styles/dayTimeline.css';
-import 'styles/dateArrowButton.css';
-import 'styles/timelineZoomSelect.css';
-import 'styles/topPanel.css';
-import 'styles/graphDisplaySelect.css';
+import 'css/main.css';
+import 'css/dropdown.css';
+import 'css/form.css';
+import 'css/timeOptionSelect.css';
+import 'css/bottomPanel.css';
+import 'css/dateNavigation.css';
+import 'css/timelineDisplaySelect.css';
+import 'css/optionsPanel.css';
+import 'css/dateSelect.css';
+import 'css/dayTimeline.css';
+import 'css/dateArrowButton.css';
+import 'css/timelineZoomSelect.css';
+import 'css/topPanel.css';
+import 'css/graphDisplaySelect.css';
 
 import App from './App';
 
