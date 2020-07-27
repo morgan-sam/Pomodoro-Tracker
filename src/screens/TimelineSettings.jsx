40290c65367b3249889d6b7bcf2aaa4562145091
Sam <samuel_morgan_@outlook.com>
2020-07-27 15:43:56 +0100

    convertCamelToCapital fn

--- src/screens/TimelineSettings.jsx
+++ src/screens/TimelineSettings.jsx
@@ -3,7 +3,7 @@ import { useHistory } from 'react-router-dom';
 import { getSystemButtonStyle } from 'styles/systemSettings';
 import { useState } from 'react';
 import Checkbox from 'components/Checkbox';
-import { splitCamelCase } from 'utility/parseText';
+import { convertCamelToCapital } from 'utility/parseText';
 
 const TimelineSettings = (props) => {
 	const { options, setOptions } = props;
@@ -48,7 +48,7 @@ const TimelineSettings = (props) => {
 	const checkWithLabelArray = () => {
 		return getBooleanObjParams(tempOptions).map((el, i) => {
 			return [
-				<div key={`title${i}`}>{splitCamelCase(el.key[el.key.length - 1])}:</div>,
+				<div key={`title${i}`}>{convertCamelToCapital(el.key[el.key.length - 1])}:</div>,
 				<Checkbox
 					key={`check${i}`}
 					style={{ borderRadius: '100%', margin: '1rem' }}
--- src/utility/parseText.js
+++ src/utility/parseText.js
@@ -1,5 +1,4 @@
 export const capitalizeFirstLetter = (str) => checkIfStr(str)[0].toUpperCase() + checkIfStr(str).slice(1);
-
-export const splitCamelCase = (str) => checkIfStr(str).split(/(?=[A-Z])/).join(' ');
-
+const splitCamelCase = (str) => checkIfStr(str).split(/(?=[A-Z])/).join(' ');
+export const convertCamelToCapital = (str) => capitalizeFirstLetter(splitCamelCase(str));
 const checkIfStr = (str) => (typeof str === 'string' && str.length > 0 ? str : 'ERROR');
