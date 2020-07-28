0ce7e939abfccb6828922e8ed8cd90dbcc5a7708
Sam <samuel_morgan_@outlook.com>
2020-07-28 09:55:03 +0100

    Changed getSystemButtonStyle settings, switch view skeleton btn

--- src/components/GraphPanel.jsx
+++ src/components/GraphPanel.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import GraphCanvas from 'components/GraphCanvas';
+import { getSystemButtonStyle } from 'styles/settings';
 
 function GraphPanel(props) {
+	const darkTheme = props.options.darkTheme;
 	return (
-		<div className={'canvasContainer'} style={props.style}>
+		<div className={'canvas-container'} style={props.style}>
+			<div className={'switch-graph-view-btn'} style={getSystemButtonStyle(darkTheme)}>
+				Switch View
+			</div>
 			<GraphCanvas
 				entriesData={props.entriesData}
 				date={props.date}
--- src/styles/settings.js
+++ src/styles/settings.js
@@ -1,5 +1,8 @@
 export const getSystemButtonStyle = (darkTheme) => {
 	return {
+		backgroundColor: 'rgb(239, 239, 239)',
+		textAlign: 'center',
+		color: 'black',
 		padding: '0.5rem 1rem',
 		borderRadius: '5px',
 		border: darkTheme ? 'none' : '1px solid #ccc',
