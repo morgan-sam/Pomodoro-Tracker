716f824fc24abfeea1fb03079539a37acdea675e
Sam <samuel_morgan_@outlook.com>
2020-07-28 16:13:33 +0100

    top-right-btns-container class style

--- src/components/TopRightButtons.jsx
+++ src/components/TopRightButtons.jsx
@@ -3,18 +3,12 @@ import firebase from 'config/firebase';
 import { useHistory } from 'react-router-dom';
 import { getSystemButtonStyle } from 'styles/settings';
 
-const btnContainerStyle = {
-	position: 'absolute',
-	top: '1rem',
-	right: '1rem'
-};
-
 const TopRightButtons = (props) => {
 	const history = useHistory();
 	const darkTheme = props.options.darkTheme;
 	const topRightButtonStyle = { ...getSystemButtonStyle(darkTheme), marginLeft: '1rem' };
 	return (
-		<div style={btnContainerStyle}>
+		<div className={'top-right-btns-container'}>
 			<button style={topRightButtonStyle} onClick={() => history.push('/script')}>
 				Get Script
 			</button>
