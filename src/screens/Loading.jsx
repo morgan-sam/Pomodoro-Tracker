53a321637ea76582a069d6fce9e69bbc6855d21b
Sam <samuel_morgan_@outlook.com>
2020-07-29 16:29:19 +0100

    ... loading animation

--- src/screens/Loading.jsx
+++ src/screens/Loading.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Loading = () => {
-	return <div className={'loading-screen'}>Loading...</div>;
+	const [ dotCount, setDotCount ] = useState(1);
+
+	useEffect(() => {
+		setTimeout(() => {
+			setDotCount(Math.max(1, (dotCount + 1) % 4));
+		}, 500);
+	});
+
+	return <div className={'loading-screen'}>{`Loading${'.'.repeat(dotCount)}`}</div>;
 };
 
 export default Loading;
