d6316148fbb265167bb03cf55d5631a84300137c
Sam <samuel_morgan_@outlook.com>
2020-07-31 21:46:50 +0100

    Timeline, graph border transition color

--- src/styles/graphPanel.js
+++ src/styles/graphPanel.js
@@ -1,9 +1,10 @@
 export const getGraphStyle = (darkTheme) => {
-    return {
-        display: 'flex',
-        border: `1px solid ${darkTheme ? 'white' : 'black'}`,
-        boxSizing: 'border-box',
-        alignItems: 'center',
-        justifyContent: 'center',
-    };
+	return {
+		display: 'flex',
+		border: `1px solid ${darkTheme ? 'white' : 'black'}`,
+		transition: 'border 1.5s',
+		boxSizing: 'border-box',
+		alignItems: 'center',
+		justifyContent: 'center'
+	};
 };
