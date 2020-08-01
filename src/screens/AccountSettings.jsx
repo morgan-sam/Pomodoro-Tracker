23a2640a0379ad55fc2208003b7e83a3e4a67a6a
Sam <samuel_morgan_@outlook.com>
2020-08-01 09:59:31 +0100

    AccountSettings uses context for darktheme

--- src/screens/AccountSettings.jsx
+++ src/screens/AccountSettings.jsx
@@ -1,18 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useHistory } from 'react-router-dom';
 import { getAppContainerStyle } from 'styles/app';
 import { getSystemButtonStyle, getSettingsBoxStyle } from 'styles/settings';
 import { accountFunctions } from 'controller/accountFunctions';
+import ThemeContext from 'context/theme';
 
-const AccountSettings = (props) => {
-	const { options } = props;
+const AccountSettings = () => {
+	const darkTheme = useContext(ThemeContext);
 	const { changePassword, changeEmail, resetAccount, deleteAccount } = accountFunctions;
 	const history = useHistory();
-	const accountButtonStyle = getSystemButtonStyle(false);
+	const accountButtonStyle = getSystemButtonStyle(darkTheme);
 
 	return (
-		<div className="screenContainer" style={getAppContainerStyle(options.darkTheme)}>
-			<div className="settingsBox" style={getSettingsBoxStyle(options.darkTheme)}>
+		<div className="screenContainer" style={getAppContainerStyle(darkTheme)}>
+			<div className="settingsBox" style={getSettingsBoxStyle(darkTheme)}>
 				<h2 className="header">Account Settings</h2>
 				<div className="accountButtonGrid">
 					<button style={accountButtonStyle} onClick={changePassword}>
