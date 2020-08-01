2fc696a4c024446253f536ddd8ace0d966da2ed1
Sam <samuel_morgan_@outlook.com>
2020-08-01 10:13:16 +0100

    accountButtons array

--- src/screens/AccountSettings.jsx
+++ src/screens/AccountSettings.jsx
@@ -11,29 +11,33 @@ const AccountSettings = () => {
 	const history = useHistory();
 	const accountButtonStyle = getSystemButtonStyle(darkTheme);
 
+	const accountButtons = [
+		<h2 className="header">Account Settings</h2>,
+		<div className="accountButtonGrid">
+			<button style={accountButtonStyle} onClick={changePassword}>
+				Change Password
+			</button>
+			<button style={accountButtonStyle} onClick={changeEmail}>
+				Change Email
+			</button>
+			<button style={accountButtonStyle} onClick={resetAccount}>
+				Reset Account
+			</button>
+			<button style={accountButtonStyle} onClick={deleteAccount}>
+				Delete Account
+			</button>
+		</div>,
+		<div className="settings-footer">
+			<button style={accountButtonStyle} onClick={() => history.push('/settings')}>
+				Return
+			</button>
+		</div>
+	];
+
 	return (
 		<div className="screenContainer" style={getAppContainerStyle(darkTheme)}>
 			<div className="settingsBox" style={getSettingsBoxStyle(darkTheme)}>
-				<h2 className="header">Account Settings</h2>
-				<div className="accountButtonGrid">
-					<button style={accountButtonStyle} onClick={changePassword}>
-						Change Password
-					</button>
-					<button style={accountButtonStyle} onClick={changeEmail}>
-						Change Email
-					</button>
-					<button style={accountButtonStyle} onClick={resetAccount}>
-						Reset Account
-					</button>
-					<button style={accountButtonStyle} onClick={deleteAccount}>
-						Delete Account
-					</button>
-				</div>
-				<div className="settings-footer">
-					<button style={accountButtonStyle} onClick={() => history.push('/settings')}>
-						Return
-					</button>
-				</div>
+				{accountButtons}
 			</div>
 		</div>
 	);
