0fbce3866a927cb64f9e150a407d9f3689ac8b9c
Sam <samuel_morgan_@outlook.com>
2020-08-01 09:50:30 +0100

    Made button/dropdown border tran instant

--- src/styles/settings.js
+++ src/styles/settings.js
@@ -6,7 +6,7 @@ export const getSystemButtonStyle = (darkTheme) => {
 		padding: '0.5rem 1rem',
 		borderRadius: '5px',
 		border: darkTheme ? '0px solid #ccc' : '1px solid #ccc',
-		transition: 'border 1.5s',
+		transition: 'border 0s 0.5s',
 		boxShadow: '2px 2px #ccc',
 		cursor: 'pointer',
 		userSelect: 'none'
