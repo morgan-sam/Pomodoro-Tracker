6212099ad2eeb985f54b8572db00109259be52e5
Sam <samuel_morgan_@outlook.com>
2020-08-01 18:17:22 +0100

    Removed console.log

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -4,7 +4,6 @@ import { AuthContext } from 'context/auth';
 
 const PrivateRoute = ({ AuthComponent, DefaultComponent, ...rest }) => {
 	const { currentUser } = useContext(AuthContext);
-	console.log(currentUser);
 	return (
 		<Route
 			{...rest}
