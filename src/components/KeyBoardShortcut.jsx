2cf1c8b6fac794448a49fde805a95914671451fc
Sam <samuel_morgan_@outlook.com>
2020-08-03 21:43:17 +0100

    Keyboard shortcut combo state

--- src/components/KeyBoardShortcut.jsx
+++ src/components/KeyBoardShortcut.jsx
@@ -1,17 +1,24 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 
 const KeyboardShortcut = () => {
+	const [ combo, setCombo ] = useState([ null, null, null ]);
+
+	if (combo === [ 'ctrl', 'alt', 'enter' ]) {
+		// something
+		setCombo([ null, null, null ]);
+	}
+
 	return (
 		<div className="keyboard-container">
-			<div className={`keyboard-key`}>
+			<div className={`keyboard-key`} onClick={() => setCombo((val) => [ ...val.slice(1), 'ctrl' ])}>
 				<code>Ctrl</code>
 			</div>
 			<div className="keyboard-add-sign">+</div>
-			<div className={`keyboard-key`}>
+			<div className={`keyboard-key`} onClick={() => setCombo((val) => [ ...val.slice(1), 'alt' ])}>
 				<code>Alt</code>
 			</div>
 			<div className="keyboard-add-sign">+</div>
-			<div className={`keyboard-key`}>
+			<div className={`keyboard-key`} onClick={() => setCombo((val) => [ ...val.slice(1), 'enter' ])}>
 				<code>Enter</code>
 			</div>
 		</div>
