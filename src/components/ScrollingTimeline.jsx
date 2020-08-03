1dd50043aa70c9d5cf65d37aad8f188ffb68a354
Sam <samuel_morgan_@outlook.com>
2020-08-03 14:26:04 +0100

    Added pomodoro draw fn

--- src/components/ScrollingTimeline.jsx
+++ src/components/ScrollingTimeline.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { useEffect, useRef } from 'react';
 import { convert24hrTo12hrTime } from 'utility/parseTime';
+import { randInt } from 'utility/random';
 
 const ScrollingTimeline = (props) => {
 	const canvasRef = useRef(null);
 
-	const hourWidth = 70;
-	const timelineHeight = 100;
+	const hourWidth = 90;
+	const timelineHeight = 120;
+
+	const color = window.getComputedStyle(document.documentElement).getPropertyValue('--color1-mid');
 
 	useEffect(() => {
 		drawTimeline();
@@ -14,12 +17,33 @@ const ScrollingTimeline = (props) => {
 
 	const drawTimeline = () => {
 		Array.from(Array(24).keys()).map((i) => drawTimeBox(i));
+		addEvents();
+	};
+
+	const addEvents = () => {
+		let curPos = 0;
+		while (curPos < hourWidth * 24) {
+			// const noOfPomodoros = randInt(1, 4);
+			drawPomodoro(curPos);
+			curPos += hourWidth / 2 + 15;
+		}
+	};
+
+	const drawPomodoro = (pos) => {
+		const ctx = canvasRef.current.getContext('2d');
+		ctx.beginPath();
+		ctx.fillStyle = color;
+		ctx.strokeStyle = 'black';
+		ctx.lineWidth = 0.2;
+		ctx.rect(pos, 30, hourWidth / 2, timelineHeight);
+		ctx.fill();
+		ctx.stroke();
 	};
 
 	const drawTimeBox = (time) => {
 		const ctx = canvasRef.current.getContext('2d');
 		ctx.beginPath();
-		ctx.rect(0, 0, hourWidth * (time + 1), timelineHeight);
+		ctx.rect(hourWidth * time, 0, hourWidth, timelineHeight);
 		ctx.font = '15px Roboto';
 		ctx.fillText(`${convert24hrTo12hrTime(time)}`, 5 + hourWidth * time, 20);
 		ctx.stroke();
