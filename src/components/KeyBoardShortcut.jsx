c1d639beab8f6f812213cb7fa800437dc5c9372a
Sam <samuel_morgan_@outlook.com>
2020-08-03 22:10:31 +0100

    Keys fall away on combo

--- src/components/KeyBoardShortcut.jsx
+++ src/components/KeyBoardShortcut.jsx
@@ -1,24 +1,25 @@
 import React, { useState } from 'react';
 
 const KeyboardShortcut = () => {
-	const [ combo, setCombo ] = useState([ null, null, null ]);
+	const [ keys, setKeys ] = useState([ null, null, null ]);
+	const [ combo, setCombo ] = useState(false);
 
-	if (combo[0] === 'ctrl' && combo[1] === 'alt' && combo[2] === 'enter') {
-		// something
-		setCombo([ null, null, null ]);
+	if (keys[0] === 'ctrl' && keys[1] === 'alt' && keys[2] === 'enter') {
+		setCombo(true);
+		setKeys([ null, null, null ]);
 	}
 
 	return (
-		<div className="keyboard-container">
-			<div className={`keyboard-key`} onClick={() => setCombo((val) => [ ...val.slice(1), 'ctrl' ])}>
+		<div className={`keyboard-container ${combo ? 'keyboard-swirl' : ''}`}>
+			<div className={`keyboard-key`} onClick={() => setKeys((val) => [ ...val.slice(1), 'ctrl' ])}>
 				<code>Ctrl</code>
 			</div>
 			<div className="keyboard-add-sign">+</div>
-			<div className={`keyboard-key`} onClick={() => setCombo((val) => [ ...val.slice(1), 'alt' ])}>
+			<div className={`keyboard-key`} onClick={() => setKeys((val) => [ ...val.slice(1), 'alt' ])}>
 				<code>Alt</code>
 			</div>
 			<div className="keyboard-add-sign">+</div>
-			<div className={`keyboard-key`} onClick={() => setCombo((val) => [ ...val.slice(1), 'enter' ])}>
+			<div className={`keyboard-key`} onClick={() => setKeys((val) => [ ...val.slice(1), 'enter' ])}>
 				<code>Enter</code>
 			</div>
 		</div>
