8c820235dcd0a90c475a770cc810310a25cd8b67
Sam <samuel_morgan_@outlook.com>
2020-08-05 15:38:29 +0100

    Removed DarkThemeContext.Consumer

--- src/context/theme.js
+++ src/context/theme.js
@@ -2,4 +2,3 @@ import React from 'react';
 
 export const DarkThemeContext = React.createContext();
 export const DarkThemeProvider = DarkThemeContext.Provider;
-const DarkThemeConsumer = DarkThemeContext.Consumer;
