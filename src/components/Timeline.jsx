d480f480cd494010316349b851b081a2496bf251
Sam <samuel_morgan_@outlook.com>
2020-08-07 23:47:02 +0100

    DrawTimeline js file

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useRef, useEffect } from 'react';
-import { convert24hrTo12hrTime } from 'utility/parseTime';
 import { DarkThemeContext, ColorThemeContext } from 'context/theme';
+import { drawTimeline, clearTimeline } from 'controller/drawTimeline';
 import { remToPx } from 'utility/convertUnit';
 
 function DayTimeline(props) {
@@ -8,89 +8,34 @@ function DayTimeline(props) {
 	const canvasRef = useRef(null);
 	const darkTheme = useContext(DarkThemeContext);
 	const colorTheme = useContext(ColorThemeContext);
+
+	const timeRange = options.timeline.endTime - options.timeline.startTime;
+	const hourWidth = remToPx(props.hourWidth);
+	const timelineHeight = 130;
+	const pomodoroWidth = 5 * hourWidth / 12;
+	const encoreWidth = hourWidth / 12;
 	const eventLengths = {
 		...props.eventLengths,
 		start: 1
 	};
 
-	const hourWidth = remToPx(props.hourWidth);
-	const pomodoroWidth = 5 * hourWidth / 12;
-	const encoreWidth = hourWidth / 12;
-
-	const timelineHeight = 130;
-	const timeRange = options.timeline.endTime - options.timeline.startTime;
-	const eventOffsetY = 40;
-
-	const drawTimeline = (ref, entries) => {
-		const context = ref.current.getContext('2d');
-
-		context.translate(0.5, 0.5);
-		drawTimelineGrid(context);
-		addEntries(context, entries);
-		drawTimelineOutline(context);
-		context.translate(-0.5, -0.5);
-	};
-
-	const addEntries = (context, entries) => {
-		entries.forEach((el) => {
-			const time = el.time.hour + el.time.minute / 60;
-			if (el.type === 'pomodoro') drawPomodoro(context, time);
-			if (el.type === 'encore') drawEncore(context, time);
-		});
-	};
-
-	const drawPomodoro = (ctx, time) => {
-		ctx.beginPath();
-		ctx.fillStyle = colorTheme.light;
-		ctx.strokeStyle = 'black';
-		ctx.lineWidth = 1;
-		const pos = (time - options.timeline.startTime) * hourWidth - pomodoroWidth;
-		ctx.rect(pos, eventOffsetY, pomodoroWidth, timelineHeight - eventOffsetY);
-		ctx.fill();
-		ctx.stroke();
-	};
-
-	const drawEncore = (ctx, time) => {
-		ctx.beginPath();
-		ctx.fillStyle = colorTheme.dark;
-		ctx.strokeStyle = 'black';
-		ctx.lineWidth = 1;
-		const pos = (time - options.timeline.startTime) * hourWidth - encoreWidth;
-		ctx.rect(pos, eventOffsetY, encoreWidth, timelineHeight - eventOffsetY);
-		ctx.fill();
-		ctx.stroke();
-	};
-
-	const clearTimeline = (ref) => {
-		const ctx = ref.current.getContext('2d');
-		ctx.fillStyle = 'white';
-		ctx.fillRect(0, 0, timeRange * hourWidth, timelineHeight);
-	};
-
-	const drawTimelineGrid = (context) => {
-		Array.from(Array(timeRange).keys()).map((i) => drawTimeBox(i, context));
-	};
-
-	const drawTimeBox = (index, ctx) => {
-		ctx.beginPath();
-		ctx.fillStyle = 'black';
-		const time = (index + options.timeline.startTime) % 24;
-		ctx.rect(hourWidth * index, 0, hourWidth, timelineHeight);
-		ctx.font = '15px Roboto';
-		ctx.fillText(`${convert24hrTo12hrTime(time)}`, 5 + hourWidth * index, 20);
-		ctx.stroke();
-	};
-
-	const drawTimelineOutline = (ctx) => {
-		ctx.beginPath();
-		ctx.rect(0, 0, hourWidth * timeRange - 1, timelineHeight - 1);
-		ctx.stroke();
-	};
-
 	useEffect(
 		() => {
-			clearTimeline(canvasRef);
-			drawTimeline(canvasRef, entries);
+			if (canvasRef.current) {
+				const timelineOptions = {
+					timeRange,
+					hourWidth,
+					pomodoroWidth,
+					encoreWidth,
+					timelineHeight,
+					eventOffsetY: 40,
+					colorTheme,
+					startTime: options.timeline.startTime,
+					context: canvasRef.current.getContext('2d')
+				};
+				clearTimeline(timelineOptions);
+				drawTimeline(timelineOptions, entries);
+			}
 		},
 		[ entries ]
 	);
---
+++ src/controller/drawTimeline.js
@@ -0,0 +1,76 @@
+import { convert24hrTo12hrTime } from 'utility/parseTime';
+
+export const drawTimeline = (options, entries) => {
+	const { context, colorTheme, timeRange, hourWidth, timelineHeight, eventOffsetY } = options;
+	context.translate(0.5, 0.5);
+	drawTimelineGrid(options);
+	addEntries(options, entries);
+	drawTimelineOutline(options);
+	context.translate(-0.5, -0.5);
+};
+
+const addEntries = (options, entries) => {
+	const { hourWidth, startTime, pomodoroWidth, encoreWidth } = options;
+	entries.forEach((el) => {
+		const time = el.time.hour + el.time.minute / 60;
+		if (el.type === 'pomodoro') {
+			const pos = (time - startTime) * hourWidth - pomodoroWidth;
+			drawPomodoro(options, pos);
+		}
+		if (el.type === 'encore') {
+			const pos = (time - startTime) * hourWidth - encoreWidth;
+			drawEncore(options, pos);
+		}
+	});
+};
+
+const drawPomodoro = (options, pos) => {
+	const { context, colorTheme, eventOffsetY, timelineHeight, pomodoroWidth } = options;
+	context.beginPath();
+	context.fillStyle = colorTheme.light;
+	context.strokeStyle = 'black';
+	context.lineWidth = 1;
+	context.rect(pos, eventOffsetY, pomodoroWidth, timelineHeight - eventOffsetY);
+	context.fill();
+	context.stroke();
+};
+
+const drawEncore = (options, pos) => {
+	const { context, colorTheme, eventOffsetY, timelineHeight, encoreWidth } = options;
+	context.beginPath();
+	context.fillStyle = colorTheme.dark;
+	context.strokeStyle = 'black';
+	context.lineWidth = 1;
+	context.rect(pos, eventOffsetY, encoreWidth, timelineHeight - eventOffsetY);
+	context.fill();
+	context.stroke();
+};
+
+export const clearTimeline = (options) => {
+	const { context, timeRange, hourWidth, timelineHeight } = options;
+	context.fillStyle = 'white';
+	context.fillRect(0, 0, timeRange * hourWidth, timelineHeight);
+};
+
+const drawTimelineGrid = (options) => {
+	const { timeRange } = options;
+	Array.from(Array(timeRange).keys()).map((i) => drawTimeBox(i, options));
+};
+
+const drawTimeBox = (index, options) => {
+	const { context, hourWidth, timelineHeight, startTime } = options;
+	context.beginPath();
+	context.fillStyle = 'black';
+	const time = (index + startTime) % 24;
+	context.rect(hourWidth * index, 0, hourWidth, timelineHeight);
+	context.font = '15px Roboto';
+	context.fillText(`${convert24hrTo12hrTime(time)}`, 5 + hourWidth * index, 20);
+	context.stroke();
+};
+
+const drawTimelineOutline = (options) => {
+	const { context, hourWidth, timeRange, timelineHeight } = options;
+	context.beginPath();
+	context.rect(0, 0, hourWidth * timeRange - 1, timelineHeight - 1);
+	context.stroke();
+};
