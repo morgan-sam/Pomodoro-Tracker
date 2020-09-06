3aeeb73dda39b6a799425401c075574c89fc1d47
Sam <samuel_morgan_@outlook.com>
2020-09-06 13:51:11 +0100

    Fixed bash script error

--- src/scripts/pomodoro.sh
+++ src/scripts/pomodoro.sh
@@ -18,7 +18,7 @@ while [ "$date1" -ge `date +%s` ]; do
 done
 clear
 notify-send "Pomodoro Complete" "$(echo -e "25 minutes have passed")"
-TIME="$(date -Iseconds)Z";
+TIME="$(date -Iseconds)";
 ENTRY='{"type":"pomodoro","date":"'${TIME::-6}.000Z'"}';
 curl -X POST -d $ENTRY "https://pomodoro-tracker-db95f.firebaseio.com/users/${localId}/events.json?auth=${idToken}"
 seconds=300; date1=$((`date +%s` + $seconds)); 
@@ -27,6 +27,6 @@ while [ "$date1" -ge `date +%s` ]; do
   echo -ne "$(date -u --date @$(($date1 - `date +%s` )) +%H:%M:%S)\r"; 
 done
 notify-send "Encore Complete" "$(echo -e "5 minutes have passed")"
-TIME="$(date -Iseconds)Z";
+TIME="$(date -Iseconds)";
 ENTRY='{"type":"encore","date":"'${TIME::-6}.000Z'"}';
 curl -X POST -d $ENTRY "https://pomodoro-tracker-db95f.firebaseio.com/users/${localId}/events.json?auth=${idToken}"
