f757f511b6d2fdd00810f1b7bb35d60669da208a
user <samuel_morgan_@outlook.com>
2021-10-12 21:13:54 -0400

    renamed config vars to not stupid names

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,14 +1,14 @@
-import * as firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/database';
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
 
 const firebaseApp = firebase.initializeApp({
-	apiKey: process.env.REACT_APP_FIREBASE_KEY,
-	authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
-	databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
-	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
 });
 
 export default firebaseApp;
