c5b18ac3520cd6cef7afb547287c535344310f5c
user <samuel_morgan_@outlook.com>
2023-03-28 10:24:01 -0300

    month overview has email count

--- src/components/MonthOverview.jsx
+++ src/components/MonthOverview.jsx
@@ -8,23 +8,37 @@ import EmailSvg from "img/email.svg";
 function MonthOverview(props) {
   const { date, entriesData, outreachData } = props;
   const colorTheme = useContext(ColorThemeContext);
-  console.log(colorTheme);
-  console.log(outreachData);
-
-  const getMonthPomodoroObj = () => {
+  
+  const getMonthPomodoroObj = (date, entriesData) => {
     let thisMonthsPomodorosObject = Object.fromEntries(
       date ? Array(getDaysInMonth(date.month, date.year)).fill(0).map((el, i) => [i + 1, 0]) : []
     );  
     entriesData.filter(event => {
       if (event.date.month === date.month && event.date.year === date.year && event.type === 'pomodoro') {
-          const day = event.date.day;
-          if (thisMonthsPomodorosObject[day]  === undefined) thisMonthsPomodorosObject[day] = 0;
-          else thisMonthsPomodorosObject[day] = thisMonthsPomodorosObject[day] + 1;
+        const day = event.date.day;
+        if (thisMonthsPomodorosObject[day]  === undefined) thisMonthsPomodorosObject[day] = 0;
+        else thisMonthsPomodorosObject[day] = thisMonthsPomodorosObject[day] + 1;
       }
     });
     return thisMonthsPomodorosObject;
   }
+  
+  const getMonthOutreachObj = (date, outreachData) => {
+    let thisMonthsOutreachObject = Object.fromEntries(
+      date ? Array(getDaysInMonth(date.month, date.year)).fill(0).map((el, i) => [i + 1, 0]) : []
+    );  
+    Object.entries(outreachData).forEach(([key, value]) => {
+      if (key.includes(`${date.year}-${twoLeadingZeroes(date.month)}`)) {
+        thisMonthsOutreachObject[parseInt(key.split('-')[2])] = value;
+      }
+    });
+    return thisMonthsOutreachObject;
+  }
+   
+  
+  
   const thisMonthsPomodorosObject = getMonthPomodoroObj(date, entriesData);
+  const thisMonthsOutreachObject = getMonthOutreachObj(date, outreachData);
   const starOfMonthWeekdayOffset = new Date(`${date.year}-${date.month}-${1}`).getDay();
 
   return (
@@ -49,7 +63,7 @@ function MonthOverview(props) {
               <div className="pomodoro-count">
                 {twoLeadingZeroes(thisMonthsPomodorosObject[day])} <span className="pomodoro-symbol" style={pomodoroSymbolStyle}></span>
               </div>
-              <div className="outreach-count">00 <img className="email-symbol" src={EmailSvg} alt="email" /></div>
+              <div className="outreach-count">{twoLeadingZeroes(thisMonthsOutreachObject[day])} <img className="email-symbol" src={EmailSvg} alt="email" /></div>
             </div>
           )
         })}
